3e902dd03ceef6db75964132fb4d6afbf3031b10
Jonas Ådahl <jadahl@gmail.com>
2023-11-09 13:03:55 +0800

    clutter/paint-context: Add content and target color state tracking
    The initial target color state is the color state of the stage view
    being painted to. If we're painting to an arbitrary framebuffer, it's
    currently hard coded to sRGB/electrical.
    
    The content color state is not set on construction, but when starting to
    paint, it's set to the color state of the stage itself. Whenever an
    actor is painted, it'll set the color state to the color state of
    itself. The intention is that offscreen rendering pushes a target color
    state that causes painting to it to not necessarily be in the stage view
    color state.
    
    Nothing is actually using this yet, but will eventually.

--- clutter/clutter/clutter-paint-context.h
+++ clutter/clutter/clutter-paint-context.h
@@ -77,4 +77,18 @@ ClutterPaintFlag clutter_paint_context_get_paint_flags (ClutterPaintContext *pai
 CLUTTER_EXPORT
 ClutterFrame * clutter_paint_context_get_frame (ClutterPaintContext *paint_context);
 
+CLUTTER_EXPORT
+void clutter_paint_context_push_color_state (ClutterPaintContext *paint_context,
+                                             ClutterColorState   *color_state);
+
+CLUTTER_EXPORT
+void clutter_paint_context_pop_color_state (ClutterPaintContext *paint_context,
+                                            ClutterColorState   *color_state);
+
+CLUTTER_EXPORT
+ClutterColorState * clutter_paint_context_get_target_color_state (ClutterPaintContext *paint_context);
+
+CLUTTER_EXPORT
+ClutterColorState * clutter_paint_context_get_color_state (ClutterPaintContext *paint_context);
+
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (ClutterPaintContext, clutter_paint_context_unref)
--- clutter/clutter/clutter-stage.c
+++ clutter/clutter/clutter-stage.c
@@ -455,10 +455,16 @@ clutter_stage_do_paint_view (ClutterStage     *stage,
 
   fb = clutter_stage_view_get_framebuffer (view);
 
+  clutter_paint_context_push_color_state (paint_context,
+                                          clutter_actor_get_color_state (CLUTTER_ACTOR (stage)));
+
   root_node = clutter_root_node_new (fb, &bg_color, COGL_BUFFER_BIT_DEPTH);
   clutter_paint_node_set_static_name (root_node, "Stage (root)");
   clutter_paint_node_paint (root_node, paint_context);
   clutter_paint_node_unref (root_node);
+
+  clutter_paint_context_pop_color_state (paint_context,
+                                         clutter_actor_get_color_state (CLUTTER_ACTOR (stage)));
 
   clutter_actor_paint (CLUTTER_ACTOR (stage), paint_context);
   clutter_paint_context_destroy (paint_context);
