fa7ead3936d8d8e0b707ac53d249768c693824a7
Joan Torres <joan.torres@suse.com>
2025-04-03 14:20:34 +0200

    compositor-view/native: Allow scan out surface when color state mismatch
    The surface will need a color transformation which will happen through
    the DRM color pipelne property.

--- src/compositor/meta-compositor-view-native.c
+++ src/compositor/meta-compositor-view-native.c
@@ -159,8 +159,6 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
   ClutterActorBox actor_box;
   MetaSurfaceActor *surface_actor;
   MetaSurfaceActorWayland *surface_actor_wayland;
-  ClutterColorState *output_color_state;
-  ClutterColorState *surface_color_state;
   MetaWaylandSurface *surface;
 
   if (meta_get_debug_paint_flags () & META_DEBUG_PAINT_DISABLE_DIRECT_SCANOUT)
@@ -287,18 +285,6 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
       meta_topic (META_DEBUG_RENDER,
                   "No direct scanout candidate: window-actor has no scanout "
                   "candidate");
-      return FALSE;
-    }
-
-  output_color_state =
-    clutter_stage_view_get_output_color_state (CLUTTER_STAGE_VIEW (view));
-  surface_color_state =
-    clutter_actor_get_color_state (CLUTTER_ACTOR (surface_actor));
-  if (!clutter_color_state_equals (output_color_state, surface_color_state))
-    {
-      meta_topic (META_DEBUG_RENDER,
-                  "No direct scanout candidate: "
-                  "surface color state doesn't match the outputs");
       return FALSE;
     }
 
